b82c466038181c462d965b252a2c183650438658
Uhrbaan <uhrbaan@gmx.net>
2025-07-19 23:23:40 +0200

    Demo working on raspberrypi4b

--- pico8-arcade.py
+++ pico8-arcade.py
@@ -1,44 +1,36 @@
 from pyvirtualdisplay.smartdisplay import SmartDisplay, autocrop
 from subprocess import Popen
-import adafruit_blinka_raspberry_pi5_piomatter as piomatter
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
 import time
-import numpy as np
+from PIL import Image
 
 # setup pico-8 execution
-PICO8_EXEC = '/home/luclement/Jeux/pico-8/pico8'
-command = [PICO8_EXEC, '-windowed', '0','-splore']
+PICO8_EXEC = '/home/pico/Projects/pico8-arcade/pico-8/pico8'
+PICO8_CART = '/home/pico/Projects/pico8-arcade/test.p8.png'
+command = [PICO8_EXEC, '-windowed', '0','-run', PICO8_CART]
 
-geometry = piomatter.Geometry(
-    width=64, # width of the board in pixels
-    height=64, # height of the board in pixels
-    n_addr_lines=5, # number of address lines used (ABCDE), usually all five used for 64x64 boards
-    n_planes=10, # color depth -> bits per color
-    n_temporal_planes=0, # temporal dithering
-    rotation=piomatter.Orientation.Normal, # orientation of the board 
-    serpentine=True # tells how the panels are connected. Here it doesn't matter bc single panel.
-)
+options = RGBMatrixOptions()
+options.rows = 64 
+options.cols = 64
+options.chain_length = 1 # For a single 64x64 panel
+options.parallel = 1
+options.hardware_mapping = 'adafruit-hat' # Or 'regular' or 'rpi-pwm' etc. based on wiring
+options.gpio_slowdown = 4 # Adjust as needed for flicker vs CPU usage
+options.disable_hardware_pulsing = False # Only set True if you cannot disable audio
 
-framebuffer = np.zeros(shape=(64, 64, 3), dtype=np.uint8)
+matrix = RGBMatrix(options = options)
 
-matrix = piomatter.PioMatter(
-    colorspace=piomatter.Colorspace.RGB888Packed, # format of the pixel data
-    pinout=piomatter.Pinout.AdafruitMatrixHatBGR, # tells which pins are connected to the connector
-    framebuffer=framebuffer, # where the data to put on display is (nparray)
-    geometry=geometry # tells the driver the physical layout of the led matrix
-)
+img_matrix = Image.new("RGB", (options.cols, options.rows))
+graphics = matrix.CreateFrameCanvas() # Create a canvas for drawing
 
 # create an environment inside xvfb
 with SmartDisplay(backend="xvfb", size=(128, 128)) as display, Popen(command, env=display.env()) as process:
     while process.poll() is None: # continue while pico-8 is running
-        img = display.grab(autocrop=True)
+        img = display.grab(autocrop=False)
         if img is None: continue
-        img = img.resize((64, 64))
+        img = img.crop((0, 0, 64, 64))
 
         # show on LED Matrix
-        # framebuffer[:, :] = np.array(img) # update memory that will be displayed
-        # matrix.show()
+        graphics.SetImage(img)
+        matrix.SwapOnVSync(graphics) # Display the frame
         # time.sleep(1/30)
-
-        # show in image viewer
-        img.show()
-        time.sleep(2)
