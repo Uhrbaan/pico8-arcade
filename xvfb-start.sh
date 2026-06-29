08c0a0d5fbe180b1e4ae101d1b4cf4f44fa7a5e1
Uhrbaan <uhrbaan@gmx.net>
2026-06-29 21:24:37 +0200

    starting grabber in script

--- xvfb-start.sh
+++ xvfb-start.sh
@@ -3,13 +3,16 @@
 # --- Configuration ---
 DISPLAY_NUM="99"
 RESOLUTION="128x128x24"
-PICO8_PATH="/home/luclement/Jeux/pico-8/pico8 /home/luclement/Projets/pico8-arcade/test.p8"
+PICO8_PATH="/home/pico/pico-8/pico8_64 -splore"
 
 # 1. Start the virtual hidden display
 Xvfb :${DISPLAY_NUM} -screen 0 ${RESOLUTION} +extension GLX +render -noreset &
 
 # 2. Force PICO-8 to run completely inside display :99 and ignore your desktop desktop
 env DISPLAY=:${DISPLAY_NUM} ${PICO8_PATH} &
+
+# 3. Start the xvfb grabber to display
+./xvfb-grab
 
 # Keep script running to let you kill everything with Ctrl+C
 trap 'kill $(jobs -p)' EXIT
