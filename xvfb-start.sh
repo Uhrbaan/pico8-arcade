4bfd780ee7391a1e2b5ed9113387afc896653c70
Uhrbaan <uhrbaan@gmx.net>
2026-06-29 21:31:38 +0200

    updated launch script

--- xvfb-start.sh
+++ xvfb-start.sh
@@ -8,8 +8,16 @@ PICO8_PATH="/home/pico/pico-8/pico8_64 -splore"
 # 1. Start the virtual hidden display
 Xvfb :${DISPLAY_NUM} -screen 0 ${RESOLUTION} +extension GLX +render -noreset &
 
+echo "Waiting for Xvfb display :${DISPLAY_NUM} to initialize..."
+while [ ! -S "/tmp/.X11-unix/X${DISPLAY_NUM}" ]; do
+    sleep 0.1
+done
+echo "Xvfb is ready!"
+
 # 2. Force PICO-8 to run completely inside display :99 and ignore your desktop desktop
 env DISPLAY=:${DISPLAY_NUM} ${PICO8_PATH} &
+
+sleep 2
 
 # 3. Start the xvfb grabber to display
 ./xvfb-grab
